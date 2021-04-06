db46cc1ec1151bfc75d1130dd8ee98685f5768f5
Anatoliy <tolxpams@gmail.com>
2021-04-06 20:18:42 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Anatoliy Khramchenko on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
